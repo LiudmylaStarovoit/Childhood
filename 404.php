87bb7242a99dd4f1428e68d627e3f5d23e154d16
LiudmylaStarovoit <103971713+LiudmylaStarovoit@users.noreply.github.com>
2023-04-26 15:29:31 +0200

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+    get_header();
+?>
+
+<div class="container toys">
+    <h2 class="subtitle">
+        <a href="<?php echo get_home_url(); ?>"> Oops, We can't find that page. BACK TO HOME?</a>
+    </h2>
+</div>
+
+<?php
+    get_footer();
+?>
